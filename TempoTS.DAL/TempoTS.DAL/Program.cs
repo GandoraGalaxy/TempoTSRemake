6f641685e397b69353fd8a39bcf7bfa164ecfc5f
Samantha Burkey <sburkey@wvup.edu>
2019-12-12 22:57:27 -0500

    Added DAL projects

---
+++ TempoTS.DAL/TempoTS.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempoTS.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
