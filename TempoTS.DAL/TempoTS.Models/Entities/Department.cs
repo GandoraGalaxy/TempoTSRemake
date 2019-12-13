3fe01659730fc58a54070cbbf38dbb5424a07260
Samantha Burkey <sburkey@wvup.edu>
2019-12-12 23:29:46 -0500

    Added Departments and User models

---
+++ TempoTS.DAL/TempoTS.Models/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TempoTS.Models.Entities
+{
+    [Table("Departments", Schema = "TimeSheet")]
+    public class Department : EntityBase
+    {
+        public string Name { get; set; }
+
+        //[ForeignKey(nameof(User))]
+        //public User ManagerID { get; set; }
+    }
+}
