c76912623569e3dc4f63837a16a1a09ad24e3892
Samantha Burkey <sburkey@wvup.edu>
2019-12-13 00:02:01 -0500

    Added Payroll and TimeClock models

---
+++ TempoTS.DAL/TempoTS.Models/Entities/Payroll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TempoTS.Models.Entities
+{
+    [Table("Payroll", Schema = "TimeSheet")]
+    public class Payroll : EntityBase
+    {
+        public decimal Salary { get; set; }
+
+        public User EmployeeID { get; set; }
+    }
+}
---
+++ TempoTS.DAL/TempoTS.Models/Entities/TimeClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TempoTS.Models.Entities
+{
+    [Table("TimeClock", Schema = "TimeSheet")]
+    public class TimeClock : EntityBase
+    {
+        public DateTime ClockIn { get; set; }
+
+        public DateTime InLunch { get; set; }
+
+        public DateTime OutLunch { get; set; }
+
+        public DateTime ClockOut { get; set; }
+
+        public DateTime TotalHours { get; set; }
+
+        public User EmployeeID { get; set; }
+    }
+}
