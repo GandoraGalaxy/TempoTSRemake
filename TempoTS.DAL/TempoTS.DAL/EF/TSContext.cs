8637d8616b066f359fa8e73ca0c88de780b5d330
Samantha Burkey <sburkey@wvup.edu>
2019-12-12 23:21:11 -0500

    Added EF and began making models

---
+++ TempoTS.DAL/TempoTS.DAL/EF/TSContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using TempoTS.Models.Entities;
+
+namespace TempoTS.DAL.EF
+{
+    public class TSContext : DbContext
+    {
+        public TSContext()
+        {
+
+        }
+
+        public TSContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=TempoTSRemodel;Trusted_Connection=True;MultipleActiveResultSets=true;");
+            }
+        }
+    }
+}
