87605f64f2cebcec1763eaa1b79e84e1fad59078
Samantha Burkey <sburkey@wvup.edu>
2019-12-15 13:31:09 -0500

    Added model build method to dbcontext

--- TempoTS.DAL/TempoTS.DAL/EF/TSContext.cs
+++ TempoTS.DAL/TempoTS.DAL/EF/TSContext.cs
@@ -28,6 +28,30 @@ namespace TempoTS.DAL.EF
             }
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email).IsUnique();
+            });
+
+            modelBuilder.Entity<Payroll>(entity =>
+            {
+                entity.Property(e => e.Salary).HasColumnType("money");
+                entity.Property(e => e.TimeStamp).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<TimeClock>(entity =>
+            {
+                entity.Property(e => e.ClockIn).HasColumnType("datetime");
+                entity.Property(e => e.ClockOut).HasColumnType("datetime");
+                entity.Property(e => e.InLunch).HasColumnType("datetime");
+                entity.Property(e => e.OutLunch).HasColumnType("datetime");
+                entity.Property(e => e.TotalHours).HasColumnType("datetime")
+                    .HasComputedColumnSql("([ClockOut] - [ClockIn]) - ([OutLunch] - [InLunch])");
+            });
+        }
+
         public virtual DbSet<Department> Departments { get; set; }
         public virtual DbSet<Payroll> Payrolls { get; set; }
         public virtual DbSet<TimeClock> TimeClocks { get; set; }
