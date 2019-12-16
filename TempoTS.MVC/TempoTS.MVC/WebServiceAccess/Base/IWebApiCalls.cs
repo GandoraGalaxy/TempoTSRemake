fcda0ddb1667969c810c543ab717316baa82688a
Samantha Burkey <sburkey@wvup.edu>
2019-12-16 12:59:50 -0500

    Started web api calls

---
+++ TempoTS.MVC/TempoTS.MVC/WebServiceAccess/Base/IWebApiCalls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempoTS.MVC.WebServiceAccess.Base
+{
+    public interface IWebApiCalls
+    {
+
+    }
+}
