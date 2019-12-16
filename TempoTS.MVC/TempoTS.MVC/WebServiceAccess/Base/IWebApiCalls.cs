058cee913e3df43296e17da58e5e7a2a2bdda645
Samantha Burkey <sburkey@wvup.edu>
2019-12-16 14:10:25 -0500

    Added web service stuff

--- TempoTS.MVC/TempoTS.MVC/WebServiceAccess/Base/IWebApiCalls.cs
+++ TempoTS.MVC/TempoTS.MVC/WebServiceAccess/Base/IWebApiCalls.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TempoTS.Models.Entities;
 
 namespace TempoTS.MVC.WebServiceAccess.Base
 {
     public interface IWebApiCalls
     {
-
+        Task<IList<Department>> GetDivisionsAsync();
+        Task<IList<Payroll>> GetPayrollAsync();
+        Task<IList<TimeClock>> GetTimeClockAsync();
+        Task<IList<User>> GetUsersAsync();
+        Task<IList<Department>> UpdateDivisionsAsync();
+        Task<IList<Payroll>> UpdatePayrollAsync();
+        Task<IList<TimeClock>> UpdateTimeClockAsync();
+        Task<IList<User>> UpdateUsersAsync();
+        Task<IList<Department>> AddDivisionsAsync();
+        Task<IList<Payroll>> AddPayrollAsync();
+        Task<IList<TimeClock>> AddTimeClockAsync();
+        Task<IList<User>> AddUsersAsync();
+        Task<IList<Department>> RemoveDivisionsAsync();
+        Task<IList<Payroll>> RemovePayrollAsync();
+        Task<IList<TimeClock>> RemoveTimeClockAsync();
+        Task<IList<User>> RemoveUsersAsync();
     }
 }
---
+++ TempoTS.MVC/TempoTS.MVC/WebServiceAccess/WebApiCalls.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TempoTS.MVC.Configuration;
+using TempoTS.Models.Entities;
+using TempoTS.MVC.WebServiceAccess.Base;
+
+namespace TempoTS.MVC.WebServiceAccess
+{
+    public class WebApiCalls : WebApiCallsBase, IWebApiCalls
+    {
+        public WebApiCalls(IWebServiceLocator settings) : base(settings)
+        {
+
+        }
+
+        public Task<IList<Department>> AddDivisionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Payroll>> AddPayrollAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TimeClock>> AddTimeClockAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> AddUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Department>> GetDivisionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Payroll>> GetPayrollAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TimeClock>> GetTimeClockAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Department>> RemoveDivisionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Payroll>> RemovePayrollAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TimeClock>> RemoveTimeClockAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> RemoveUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Department>> UpdateDivisionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Payroll>> UpdatePayrollAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TimeClock>> UpdateTimeClockAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> UpdateUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
